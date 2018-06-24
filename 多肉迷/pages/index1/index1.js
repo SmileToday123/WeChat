27f9aad9c92bf684e853f686760df2686d3f5b97
SmileToday123 <437785423@qq.com>
2018-06-24 18:06:19 +0800

    首页修改

---
+++ 多肉迷/pages/index1/index1.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    imgUrls: [{ src: '/assets/image/banner.jpg', mode: 'aspectfill' },
+    { src: '/assets/image/banner2.jpg', mode: 'aspectfill' },
+    { src: '/assets/image/banner3.jpg', mode: 'aspectfill' }]
+  },
+  onLoad: function (options) {
+    // wx.request({
+    //   url: '',
+    //   success:function(result){
+    //     this.setData({
+
+    //     })
+    //   }
+    // })
+  },
+})
